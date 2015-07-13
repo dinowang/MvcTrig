1c0097708f7164f506a8650d3aa51187a605de11
dinowang <dino.cool@gmail.com>
2015-07-13 14:32:22 +0800

    Add missing BootstrapController.cs

---
+++ Hexon.MvcTrig.Sample/Controllers/BootstrapController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Hexon.MvcTrig.Bootstrap;
+
+namespace Hexon.MvcTrig.Sample.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        public ActionResult Modal()
+        {
+            this.Trig(x => x.ModalOpen());
+
+            return PartialView();
+        }
+    }
+}
